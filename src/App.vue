b792cc01deb8a3797036c2ad9abf35061dfddbf4
prateekYadav07 <yadavprateek39@gmail.com>
2022-06-13 09:52:26 +0530

    Card styling

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card">
+    <div class="title">Title</div>
+
+    <div class="content">content</div>
+
+    <div class="description">Description</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.card {
+  border: 1px solid silver;
+  border-radius: 8px;
+  max-width: 200px;
+  margin: 0 5px;
+  cursor: pointer;
+  box-shadow: 0px 1px 3px darkgrey;
+  transition: 0.2s;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+.title,
+.content,
+.description {
+  padding: 16px;
+  text-transform: capitalize;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.title,
+.content {
+  border-bottom: 1px solid silver;
+}
+.title {
+  font-size: 1.25em;
+}
+.card:hover {
+  transition: 0.2s;
+  box-shadow: 0px 1px 9px darkgrey;
 }
 </style>
